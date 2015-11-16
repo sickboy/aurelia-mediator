001c67439dfd6ba33bf3a3e1e025ba15ee7151a1
Patrick Roza <roza@sixnetworks.net>
2015-11-16 11:42:59 +0100

    update-packages

--- config.js
+++ config.js
@@ -7,32 +7,32 @@ System.config({
   },
 
   map: {
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.0",
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.1",
     "aurelia-framework": "github:aurelia/framework@0.18.0",
     "traceur": "github:jmcriffey/bower-traceur@0.0.92",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.92",
-    "github:aurelia/binding@0.11.1": {
+    "github:aurelia/binding@0.11.4": {
       "aurelia-metadata": "github:aurelia/metadata@0.10.1",
       "aurelia-pal": "github:aurelia/pal@0.3.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.9.0",
       "core-js": "npm:core-js@1.2.6"
     },
-    "github:aurelia/dependency-injection@0.12.0": {
+    "github:aurelia/dependency-injection@0.12.1": {
       "aurelia-logging": "github:aurelia/logging@0.9.0",
       "aurelia-metadata": "github:aurelia/metadata@0.10.1",
       "aurelia-pal": "github:aurelia/pal@0.3.0",
       "core-js": "npm:core-js@1.2.6"
     },
     "github:aurelia/framework@0.18.0": {
-      "aurelia-binding": "github:aurelia/binding@0.11.1",
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.0",
+      "aurelia-binding": "github:aurelia/binding@0.11.4",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.1",
       "aurelia-loader": "github:aurelia/loader@0.11.0",
       "aurelia-logging": "github:aurelia/logging@0.9.0",
       "aurelia-metadata": "github:aurelia/metadata@0.10.1",
       "aurelia-pal": "github:aurelia/pal@0.3.0",
       "aurelia-path": "github:aurelia/path@0.11.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.9.0",
-      "aurelia-templating": "github:aurelia/templating@0.17.3",
+      "aurelia-templating": "github:aurelia/templating@0.17.5",
       "core-js": "npm:core-js@1.2.6"
     },
     "github:aurelia/loader@0.11.0": {
@@ -46,9 +46,9 @@ System.config({
     "github:aurelia/task-queue@0.9.0": {
       "aurelia-pal": "github:aurelia/pal@0.3.0"
     },
-    "github:aurelia/templating@0.17.3": {
-      "aurelia-binding": "github:aurelia/binding@0.11.1",
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.0",
+    "github:aurelia/templating@0.17.5": {
+      "aurelia-binding": "github:aurelia/binding@0.11.4",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.1",
       "aurelia-loader": "github:aurelia/loader@0.11.0",
       "aurelia-logging": "github:aurelia/logging@0.9.0",
       "aurelia-metadata": "github:aurelia/metadata@0.10.1",
