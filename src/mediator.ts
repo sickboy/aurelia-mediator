14fac95bc390f01ff23bf6d346de4db68e96662d
Patrick Roza <roza@sixnetworks.net>
2016-06-15 17:04:44 +0200

    Fix: Throwing error, not string

--- src/mediator.ts
+++ src/mediator.ts
@@ -6,15 +6,15 @@ export interface IRequest<T> {
   handle(mediator: IMediator): Promise<T>;
   isReadOnly: boolean;
 }
-export interface IQuery<T> extends IRequest<T> {}
-export interface ICommand<T> extends IRequest<T> {}
-export interface IVoidCommand extends ICommand<void> {}
+export interface IQuery<T> extends IRequest<T> { }
+export interface ICommand<T> extends IRequest<T> { }
+export interface IVoidCommand extends ICommand<void> { }
 
 export interface IRequestHandler<TRequest, TResponse> {
   handle(request: TRequest): Promise<TResponse>;
 }
 
-export interface IVoidCommandHandler extends IRequestHandler<IVoidCommand, void> {}
+export interface IVoidCommandHandler extends IRequestHandler<IVoidCommand, void> { }
 
 export abstract class Request<T> implements IRequest<T> {
   isReadOnly = false;
@@ -23,8 +23,8 @@ export abstract class Request<T> implements IRequest<T> {
 export class Query<T> extends Request<T> implements IQuery<T> {
   isReadOnly = true;
 }
-export class Command<T> extends Request<T> implements ICommand<T> {}
-export class VoidCommand extends Command<void> implements IVoidCommand {}
+export class Command<T> extends Request<T> implements ICommand<T> { }
+export class VoidCommand extends Command<void> implements IVoidCommand { }
 
 export interface IMediator {
   request<T>(request: IRequest<T>): Promise<T>;
@@ -40,7 +40,7 @@ export class Mediator implements IMediator {
       var handler = Container.instance.get(Mediator.registry.get(cstr));
       return handler.handle(request);
     }
-    throw "no handler registered for this request";
+    throw new Error("no handler registered for this request");
   }
 }
 
